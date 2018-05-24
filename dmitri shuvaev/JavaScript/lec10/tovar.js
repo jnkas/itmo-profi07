2f8ef9ceb6a551bb8e2c7566cdb03e5d5cfbde90
DmitryShuvaev <shdmitrimypost@mail.ru>
2018-05-24 17:06:38 +0300

    lec10ispr

--- dmitri shuvaev/JavaScript/lec10/tovar.js
+++ dmitri shuvaev/JavaScript/lec10/tovar.js
@@ -1,4 +1,4 @@
-var tovar=function(name,price){
+var Tovar=function(name,price){
   this._name=name;
   this._price=price;
     this.info=function(){return "Имя товара "+this._name+"<br/> Цена "+this._price;}
@@ -11,10 +11,14 @@ function Korsina_tovarov(){
     this._summ=0;
     this.add=function(n,p){
         
-        this.store.push(new tovar(n,p));
+        this.store.push(new Tovar(n,p));
         this._kolichestvo++;
         this._summ+=p;
-    };
+    };//====================
+    this.addTovar=function(name,price)
+    {
+     
+    };//====================
     this.getSumm=function(){
 
         return "Сумма "+this._summ;
@@ -33,3 +37,32 @@ st1.add("tvset",10);
 st1.add("firdge",20);
 st1.printStore();
 document.write(st1.getSumm()+"<br/>"+st1.getKolichestvo());
+    //===================
+function Korsina(){
+    this.store=[];  
+    //===================
+    this.Tovar=function(name,price)
+    {
+     this._name=name;
+     this._price=price;
+     this.info=function(){return "Имя товара "+this._name+"<br/> Цена "+this._price;}
+     this.getPrice=function(){return this._price;}
+    };
+    //====================
+    this.addTovar=function(name,price)
+    {
+     var t=new this.Tovar(name,price);
+        this.store.push(t);
+    
+    };//====================
+        this.printStore=function(){
+     for(var i=0;i<this.store.length;i++)
+     document.write(this.store[i].info()+"<br/>");   
+    };
+}
+document.write("<br/>******************<br/>");
+var kors=new Korsina();
+kors.addTovar("name",111);
+kors.addTovar("table",200);
+kors.addTovar("apple",158);
+kors.printStore();
