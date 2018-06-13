d5e927856b9a1f452584df018e27635cbc26117a
DmitryShuvaev <shdmitrimypost@mail.ru>
2018-06-13 22:17:53 +0300

    lec3

---
+++ dmitri shuvaev/node/lec3/Widget/WidgetCreate.js
@@ -0,0 +1,22 @@
+﻿function WidgetCallback(JSONobject,r) {
+    var o = JSONobject;
+   var wHTML = "";
+    var tempMax=o.Temperature.Maximum.Value;
+	var icon=o.Day.Icon;
+    var wind=o.Day.Wind.Speed.Value;
+      wHTML += ('<div class="location Main" >');
+	    wHTML += ('<p class="Main" id="rf">RealFeel '+Math.round(o.RealFeelTemperature.Maximum.Value)+'°'+'</p>');
+	
+		wHTML += '<p class="Main" id="city">Komsomolsk-on-amure</p>';
+		 	wHTML += '</div>';
+   wHTML += '<p class="temperature Main" id="temp">'+Math.round(tempMax)+'°'+'</p>';
+  
+	  
+ 	wHTML += '<div class="climate_bg Main" id="cl"><img src="WeatherIcons/'+r+'.png"></div>';
+    	wHTML += '<div class="info_bg">';
+  	wHTML += '<div class="windspeed" id="wind">Wind '+wind+o.Day.Wind.Speed.Unit+'</div>';
+		wHTML += '</div>';
+     document.getElementById("container").innerHTML= wHTML;
+	 
+}
+
---
+++ dmitri shuvaev/node/lec3/Widget/data.js
@@ -0,0 +1,35 @@
+﻿
+var request=new XMLHttpRequest();
+
+request.onreadystatechange=function(){
+    if(request.readyState===4){
+        if(request.status===200){
+        var o=JSON.parse(request.response);
+        console.log(o.DailyForecasts[0].Temperature.Maximum.Value); 
+		
+            myFunction(o.DailyForecasts[0],getRandomArbitrary(1,8));
+      
+        }else{
+            throw new Error(request.response);
+        }
+
+    }
+}
+
+
+function fu(){
+ request.open('GET','/jsondata.json');
+ request.send();   
+}
+
+window.setInterval(function(){
+ fu();
+}, 1000);
+
+function myFunction(b,r) {
+WidgetCallback(b,r);
+  
+}
+function getRandomArbitrary(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
